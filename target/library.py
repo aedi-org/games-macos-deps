410f2c7a66dbb6d99757d5eb0c61a405eeb020ff
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-09-02 10:48:50 +0300

    aedi: update sdl2 to 2.32.10

--- target/library.py
+++ target/library.py
@@ -678,8 +678,8 @@ class Sdl2Target(base.CMakeStaticDependencyTarget):
 
     def prepare_source(self, state: BuildState):
         state.download_source(
-            'https://github.com/libsdl-org/SDL/releases/download/release-2.32.8/SDL2-2.32.8.tar.gz',
-            '0ca83e9c9b31e18288c7ec811108e58bac1f1bb5ec6577ad386830eac51c787e')
+            'https://github.com/libsdl-org/SDL/releases/download/release-2.32.10/SDL2-2.32.10.tar.gz',
+            '5f5993c530f084535c65a6879e9b26ad441169b3e25d789d83287040a9ca5165')
 
     def configure(self, state: BuildState):
         opts = state.options
