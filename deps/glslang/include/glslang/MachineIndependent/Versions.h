59cfa178469d4c8a4cd06773db434507c9e3487b
alexey.lysiuk <alexey.lysiuk@gmail.com>
2026-05-02 12:04:06 +0300

    deps: update glslang to 16.3.0

--- deps/glslang/include/glslang/MachineIndependent/Versions.h
+++ deps/glslang/include/glslang/MachineIndependent/Versions.h
@@ -186,6 +186,7 @@ const char* const E_GL_EXT_shader_image_load_formatted = "GL_EXT_shader_image_lo
 
 const char* const E_GL_EXT_shader_16bit_storage             = "GL_EXT_shader_16bit_storage";
 const char* const E_GL_EXT_shader_8bit_storage              = "GL_EXT_shader_8bit_storage";
+const char* const E_GL_EXT_abort                            = "GL_EXT_abort";
 
 
 // EXT extensions
@@ -280,6 +281,7 @@ const char* const E_GL_NV_shading_rate_image                    = "GL_NV_shading
 const char* const E_GL_NV_ray_tracing                           = "GL_NV_ray_tracing";
 const char* const E_GL_NV_ray_tracing_motion_blur               = "GL_NV_ray_tracing_motion_blur";
 const char* const E_GL_NV_fragment_shader_barycentric           = "GL_NV_fragment_shader_barycentric";
+const char* const E_GL_KHR_compute_shader_derivatives           = "GL_KHR_compute_shader_derivatives";
 const char* const E_GL_NV_compute_shader_derivatives            = "GL_NV_compute_shader_derivatives";
 const char* const E_GL_NV_shader_texture_footprint              = "GL_NV_shader_texture_footprint";
 const char* const E_GL_NV_mesh_shader                           = "GL_NV_mesh_shader";
@@ -296,10 +298,14 @@ const char* const E_GL_NV_cluster_acceleration_structure        = "GL_NV_cluster
 const char* const E_GL_NV_linear_swept_spheres                  = "GL_NV_linear_swept_spheres";
 const char* const E_GL_NV_gpu_shader5                           = "GL_NV_gpu_shader5";
 const char* const E_GL_NV_push_constant_bank                    = "GL_NV_push_constant_bank";
+const char* const E_GL_NV_explicit_typecast                     = "GL_NV_explicit_typecast";
 
 // ARM
 const char* const E_GL_ARM_shader_core_builtins                 = "GL_ARM_shader_core_builtins";
 const char* const E_GL_ARM_tensors                              = "GL_ARM_tensors";
+const char* const E_GL_ARM_tensors_bfloat16                     = "GL_ARM_tensors_bfloat16";
+const char* const E_GL_ARM_tensors_float_e5m2                   = "GL_ARM_tensors_float_e5m2";
+const char* const E_GL_ARM_tensors_float_e4m3                   = "GL_ARM_tensors_float_e4m3";
 
 // Arrays of extensions for the above viewportEXTs duplications
 
