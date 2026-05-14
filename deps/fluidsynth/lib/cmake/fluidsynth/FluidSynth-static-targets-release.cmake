c163c257dd97ce8a0663d05086824aed8d627da7
alexey.lysiuk <alexey.lysiuk@gmail.com>
2026-05-14 13:11:46 +0300

    deps: remove references to fluidsynth binary

--- deps/fluidsynth/lib/cmake/fluidsynth/FluidSynth-static-targets-release.cmake
+++ deps/fluidsynth/lib/cmake/fluidsynth/FluidSynth-static-targets-release.cmake
@@ -7,12 +7,8 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 
 # Import target "FluidSynth::fluidsynth" for configuration "Release"
 set_property(TARGET FluidSynth::fluidsynth APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(FluidSynth::fluidsynth PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/fluidsynth"
-  )
 
 list(APPEND _cmake_import_check_targets FluidSynth::fluidsynth )
-list(APPEND _cmake_import_check_files_for_FluidSynth::fluidsynth "${_IMPORT_PREFIX}/bin/fluidsynth" )
 
 # Import target "FluidSynth::libfluidsynth" for configuration "Release"
 set_property(TARGET FluidSynth::libfluidsynth APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
