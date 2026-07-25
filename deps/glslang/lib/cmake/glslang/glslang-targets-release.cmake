599640376f0200ea4dc6ff815fc167fc643b50c3
alexey.lysiuk <alexey.lysiuk@gmail.com>
2026-07-25 12:10:15 +0300

    deps: update glslang to 16.4.0

--- deps/glslang/lib/cmake/glslang/glslang-targets-release.cmake
+++ deps/glslang/lib/cmake/glslang/glslang-targets-release.cmake
@@ -8,32 +8,32 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "glslang::SPIRV" for configuration "Release"
 set_property(TARGET glslang::SPIRV APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(glslang::SPIRV PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libSPIRV.16.3.0.dylib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libSPIRV.16.4.0.dylib"
   IMPORTED_SONAME_RELEASE "@rpath/libSPIRV.16.dylib"
   )
 
 list(APPEND _cmake_import_check_targets glslang::SPIRV )
-list(APPEND _cmake_import_check_files_for_glslang::SPIRV "${_IMPORT_PREFIX}/lib/libSPIRV.16.3.0.dylib" )
+list(APPEND _cmake_import_check_files_for_glslang::SPIRV "${_IMPORT_PREFIX}/lib/libSPIRV.16.4.0.dylib" )
 
 # Import target "glslang::glslang" for configuration "Release"
 set_property(TARGET glslang::glslang APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(glslang::glslang PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libglslang.16.3.0.dylib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libglslang.16.4.0.dylib"
   IMPORTED_SONAME_RELEASE "@rpath/libglslang.16.dylib"
   )
 
 list(APPEND _cmake_import_check_targets glslang::glslang )
-list(APPEND _cmake_import_check_files_for_glslang::glslang "${_IMPORT_PREFIX}/lib/libglslang.16.3.0.dylib" )
+list(APPEND _cmake_import_check_files_for_glslang::glslang "${_IMPORT_PREFIX}/lib/libglslang.16.4.0.dylib" )
 
 # Import target "glslang::glslang-default-resource-limits" for configuration "Release"
 set_property(TARGET glslang::glslang-default-resource-limits APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(glslang::glslang-default-resource-limits PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libglslang-default-resource-limits.16.3.0.dylib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libglslang-default-resource-limits.16.4.0.dylib"
   IMPORTED_SONAME_RELEASE "@rpath/libglslang-default-resource-limits.16.dylib"
   )
 
 list(APPEND _cmake_import_check_targets glslang::glslang-default-resource-limits )
-list(APPEND _cmake_import_check_files_for_glslang::glslang-default-resource-limits "${_IMPORT_PREFIX}/lib/libglslang-default-resource-limits.16.3.0.dylib" )
+list(APPEND _cmake_import_check_files_for_glslang::glslang-default-resource-limits "${_IMPORT_PREFIX}/lib/libglslang-default-resource-limits.16.4.0.dylib" )
 
 # Import target "glslang::glslang-standalone" for configuration "Release"
 set_property(TARGET glslang::glslang-standalone APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
